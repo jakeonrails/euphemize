f80b57a2fff7d473e5d4b0c9151174680c872b48
Jake Moffatt <jakeonrails@gmail.com>
2011-02-08 22:49:54 -0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Euphemize::Application.config.secret_token = 'bbba64201ba6ffa7ca78714ae5c1b80d02c8e76eada00d9c7d8e04c3b4a8e74025d1768edc7fce13698c77dbe162875061ec35ab8df8330f3485ca367449640d'
